ecbebee8ecf6c3541702ca5a7c90366001c307ec
kierunb <kierunb@gmail.com>
2021-11-24 12:14:20 +0100

    Add project files.

---
+++ WebAppMVC5DocX/Controllers/HomeController.cs
@@ -0,0 +1,79 @@
+﻿using DocumentFormat.OpenXml.Packaging;
+using OpenXmlPowerTools;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Xml.Linq;
+
+namespace WebAppMVC5DocX.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Docx()
+        {
+            string filePath = Server.MapPath(@"~/Documents/test-docx.docx");
+            
+            byte[] byteArray = System.IO.File.ReadAllBytes(filePath);
+
+            using (var memoryStream = new MemoryStream())
+            {
+                memoryStream.Write(byteArray, 0, byteArray.Length);
+                using (var doc = WordprocessingDocument.Open(memoryStream, true))
+                {
+                    var settings = new HtmlConverterSettings()
+                    {
+                        PageTitle = "My Page Title"
+                    };
+                    var html = HtmlConverter.ConvertToHtml(doc, settings);
+
+                    ViewBag.HtmlOutput = html.ToString();
+
+                    //File.WriteAllText(HTMLFilePath, html.ToStringNewLineOnAttributes());
+                }
+            }
+
+
+            return View();
+        }
+
+
+
+
+        public ActionResult Pdf()
+        {
+
+            return View();
+        }
+
+        public ActionResult GetPdf()
+        {
+
+            string filePath = Server.MapPath(@"~/Documents/test-pdf.pdf");
+
+            byte[] byteArray = System.IO.File.ReadAllBytes(filePath);
+
+            return File(byteArray, "application/pdf");
+        }
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ WebAppMVC5DocX/Readme.md
@@ -0,0 +1,29 @@
+﻿
+#### Lame example of how to render docx and pdf files in html in asp.net
+
+**Tech stack**
+- ASP.NET MVC 5 (.NET Framework)
+- Open XML Power Tools
+  - There is a .NET Standard version of this library, so should work with .NET Core/.NET 5-6 too.
+- pdf.js
+  - Will work with any web framework - it's js
+
+
+**DocX -> HTML**
+- https://github.com/EricWhiteDev/Open-Xml-PowerTools
+- https://stackoverflow.com/questions/2266097/convert-from-word-document-to-html
+- https://stackoverflow.com/questions/564650/convert-html-to-pdf-in-net
+- https://stackoverflow.com/questions/8123786/converting-from-pdf-to-html
+
+**Pdf to HTML**
+- https://mozilla.github.io/pdf.js/
+- https://mozilla.github.io/pdf.js/examples/
+
+
+**Free PDF generation library**
+- https://github.com/QuestPDF/QuestPDF
+- https://www.questpdf.com/documentation/#overview
+
+**Free DOCX generation library (.NET Standard)**
+- https://github.com/wtulloch/DocX
+- https://www.nuget.org/packages/DocXCore/
